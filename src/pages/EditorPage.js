b7c42f9cae6accae5d49d5651aa186e184451588
김흥열 <favianred@gmail.com>
2018-12-02 02:43:32 +0900

    * 20.5 에디터 페이지 UI 구현

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import EditorTemplate from 'components/editor/EditorTemplate';
+import EditorHeader from 'components/editor/EditorHeader';
+import EditorPane from 'components/editor/EditorPane';
 
 const EditorPage = () => {
   return (
-    <div>
-      Editor
-    </div>
+    <EditorTemplate
+      header={<EditorHeader/>}
+      editor={<EditorPane/>}
+    />
   );
 };
 
