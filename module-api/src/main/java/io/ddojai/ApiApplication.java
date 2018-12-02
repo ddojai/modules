2700f3e02bcf6365544b418462421a9502a79a22
김흥열 <favianred@gmail.com>
2018-12-02 20:57:59 +0900

    * 블로그 참조   * https://jojoldu.tistory.com   * 스프링 부트로 웹 서비스 출시하기 1~3

--- module-api/src/main/java/io/ddojai/ApiApplication.java
+++ module-api/src/main/java/io/ddojai/ApiApplication.java
@@ -2,7 +2,9 @@ package io.ddojai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing      // JPA Auditing 활성화
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
---
+++ module-api/src/main/java/io/ddojai/controller/WebRestController.java
@@ -0,0 +1,34 @@
+package io.ddojai.controller;
+
+import io.ddojai.posts.PostsMainResponseDto;
+import io.ddojai.posts.PostsSaveRequestDto;
+import io.ddojai.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @GetMapping("/")
+    public List<PostsMainResponseDto> main() {
+        return postsService.findAllDesc();
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
+    }
+}
