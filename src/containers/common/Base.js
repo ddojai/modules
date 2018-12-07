96a2f11e3b5d96c07998c98cee4b5de048401061
김흥열 <favianred@gmail.com>
2018-12-08 03:22:25 +0900

    * checkLogin 구현   * Base.js     * 새로 고침시 로그인 체크 호출   * api.js     * getUser() 추가   * base.js     * CHECK_LOGIN 리듀서 구현

--- src/containers/common/Base.js
+++ src/containers/common/Base.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as baseActions from 'store/modules/base';
+import { ACCESS_TOKEN } from "commonConstants";
 
 class Base extends Component {
   initialize = async () => {
     const { BaseActions } = this.props;
-    BaseActions.checkLogin();
+    if (localStorage.getItem(ACCESS_TOKEN)) {
+      BaseActions.checkLogin();
+    }
   };
 
   componentDidMount() {
--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -1,6 +1,6 @@
 import { createAction, handleActions } from "redux-actions";
 
-import { Map } from "immutable";
+import { Map, fromJS } from "immutable";
 import { pender } from "redux-pender";
 import * as api from 'lib/api';
 
@@ -14,7 +14,7 @@ const CHECK_LOGIN = 'base/CHECK_LOGIN';
 // action creators
 export const showModal = createAction(SHOW_MODAL);
 export const hideModal = createAction(HIDE_MODAL);
-export const checkLogin = createAction(CHECK_LOGIN);
+export const checkLogin = createAction(CHECK_LOGIN, api.getUser);
 export const logout = createAction(LOGOUT);
 
 
@@ -26,7 +26,7 @@ const initialState = Map({
     login: false  // 추후 구현될 로그인 모달
   }),
   authenticated: false, // 현재 로그인 상태
-  currentUser: null,    // 로그인 정보
+  currentUser: Map({}),    // 로그인 정보
 });
 
 // reducer
@@ -39,21 +39,19 @@ export default handleActions({
     const { payload: modalName } = action;
     return state.setIn(['modal', modalName], false);
   },
-  [CHECK_LOGIN]: (state, action) => {
-    // api.getCurrentUser()
-    //   .then(response => {
-    //     console.log(response);
-    //     return state
-    //       // .set("currentUser", response)
-    //       .set("authenticated", true)
-    //   }).catch(error => {
-    //   console.log(error);
-    // });
-    return state.set("authenticated", true)
-  },
+  ...pender({
+    type: CHECK_LOGIN,
+    onSuccess: (state, action) => {
+      const { data: currentUser } = action.payload;
+      console.log(currentUser);
+      return state
+        .set("currentUser", fromJS(currentUser))
+        .set("authenticated", true)
+    }
+  }),
   [LOGOUT]: (state, action) => {
     return state
-      .set("currentUser", null)
+      .set("currentUser", Map({}))
       .set("authenticated", false)
   }
 }, initialState);
