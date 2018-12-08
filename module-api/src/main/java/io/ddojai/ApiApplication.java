fa9880465bdb57b1994f675c69aa54b36e8a83b0
김흥열 <favianred@gmail.com>
2018-12-09 00:13:40 +0900

    * OAuth2   * jjwt 의존성 공통으로 변경     * build.gradle   * AppProperties 바인딩, 활성화     * AppProperties.java     * ApiApplication.java   * cors 사용     * WebMvcConfig.java    * 데이터베이스 엔터티 추가     * AuthProvider.java     * User.java   * 리포지토리 추가     * UserRepository.java
    * 참고 : https://www.callicoder.com/spring-boot-security-oauth2-social-login-part-1/

--- module-api/src/main/java/io/ddojai/ApiApplication.java
+++ module-api/src/main/java/io/ddojai/ApiApplication.java
@@ -1,11 +1,14 @@
 package io.ddojai;
 
+import io.ddojai.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing      // JPA Auditing 활성화
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
