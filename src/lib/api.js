6a2d3f14a2a2440d4876a41bff69f9894a42dc61
김흥열 <favianred@gmail.com>
2018-12-09 01:56:30 +0900

    * 헤더 토큰 추가   * api.js     * writePost, editPost, removePost

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,14 +3,16 @@ import queryString from "query-string";
 import { ACCESS_TOKEN } from "commonConstants";
 
 // post
-export const writePost = ({ title, content, tags }) =>
-  axios.post("/api/posts", { title, content, tags });
-export const getPost = id => axios.get(`/api/posts/${id}`);
+export const getPost = id =>
+  axios.get(`/api/posts/${id}`);
 export const getPostList = ({ tag, page }) =>
   axios.get(`/api/posts/?${queryString.stringify({ tag, page })}`);
+export const writePost = ({ title, content, tags }) =>
+  axios.post("/api/posts", { title, content, tags }, { headers: { Authorization: 'Bearer ' + localStorage.getItem(ACCESS_TOKEN) } });
 export const editPost = ({ id, title, content, tags }) =>
-  axios.patch(`/api/posts/${id}`, { title, content, tags });
-export const removePost = id => axios.delete(`/api/posts/${id}`);
+  axios.patch(`/api/posts/${id}`, { title, content, tags }, { headers: { Authorization: 'Bearer ' + localStorage.getItem(ACCESS_TOKEN) } });
+export const removePost = id =>
+  axios.delete(`/api/posts/${id}`, { headers: { Authorization: 'Bearer ' + localStorage.getItem(ACCESS_TOKEN) } });
 
 // auth
 export const getUser = () =>
