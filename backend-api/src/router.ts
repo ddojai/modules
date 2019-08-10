2d8551c32f3b822f2838e6d090c895b139d9a942
ddojai <favianred@gmail.com>
2019-08-10 20:53:31 +0900

    * Remove backend-api

--- backend-api/src/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-
-router.use('/', require('./controllers/home'))
-router.use('/posts', require('./controllers/posts'))
-
-module.exports = router
--- backend-api/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Err } from './interfaces'
-
-export function error(status: number, message: string, data?: any): Error {
-  let err = new Error(message) as Err
-  err.status = status
-  err.data = data
-  return err
-}
