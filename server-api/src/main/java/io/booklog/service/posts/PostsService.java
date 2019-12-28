2382dd7ab55fca26d42547b14e44e7ed88bc130c
ddojai <favianred@gmail.com>
2019-12-28 21:46:59 +0900

    * 스프링 부트와 AWS로 혼자 구현하는 웹 서비스   * 3장 스프링 부트에서 JPA로 데이터베이스 다뤄보자

---
+++ server-api/src/main/java/io/booklog/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package io.booklog.service.posts;
+
+import io.booklog.domain.posts.Posts;
+import io.booklog.domain.posts.PostsRepository;
+import io.booklog.web.dto.PostsResponseDto;
+import io.booklog.web.dto.PostsSaveRequestDto;
+import io.booklog.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+
+    public PostsResponseDto findByid(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ server-api/src/main/java/io/booklog/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package io.booklog.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
