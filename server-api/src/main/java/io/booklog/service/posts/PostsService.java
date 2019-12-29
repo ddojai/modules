8d292590fb9148c92dff0b91b46c94ff6107bb5e
ddojai <favianred@gmail.com>
2019-12-29 12:12:19 +0900

    * 스프링 부트와 AWS로 혼자 구현하는 웹 서비스   * 4장 머스테치로 화면 구성하기

--- server-api/src/main/java/io/booklog/service/posts/PostsService.java
+++ server-api/src/main/java/io/booklog/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package io.booklog.service.posts;
 
 import io.booklog.domain.posts.Posts;
 import io.booklog.domain.posts.PostsRepository;
+import io.booklog.web.dto.PostsListResponseDto;
 import io.booklog.web.dto.PostsResponseDto;
 import io.booklog.web.dto.PostsSaveRequestDto;
 import io.booklog.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +31,24 @@ public class PostsService {
         return id;
     }
 
-
     public PostsResponseDto findByid(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
