3bfff0260979ba62a97010f4646e49176fa79b41
ddojai <favianred@gmail.com>
2020-03-01 01:33:24 +0900

    * 27.1 포스트 수정

--- frontend/src/components/write/WriteActionButtons.js
+++ frontend/src/components/write/WriteActionButtons.js
@@ -18,15 +18,15 @@ const StyledButton = styled(Button)`
   }
 `;
 
-const WriteActionButtons = ({ onCancel, onPublish }) => {
-    return (
-      <WriteActionButtonsBlock>
-        <StyledButton cyan onClick={onPublish}>
-          포스트 등록
-        </StyledButton>
-        <StyledButton onClick={onCancel}>취소</StyledButton>
-      </WriteActionButtonsBlock>
-    );
+const WriteActionButtons = ({ onCancel, onPublish, isEdit }) => {
+  return (
+    <WriteActionButtonsBlock>
+      <StyledButton cyan onClick={onPublish}>
+        포스트 등록 {isEdit ? '수정' : '등록'}
+      </StyledButton>
+      <StyledButton onClick={onCancel}>취소</StyledButton>
+    </WriteActionButtonsBlock>
+  );
 };
 
 export default WriteActionButtons;
