bbebe3459a18e46df00478dba306ed622c5b3c4f
ddojai <favianred@gmail.com>
2020-09-02 19:19:07 +0900

    Add application-datasource to resources

--- backend-api/src/main/java/com/movement/life/Application.java
+++ backend-api/src/main/java/com/movement/life/Application.java
@@ -12,7 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
   private static final String APPLICATION_LOCATIONS = "spring.config.location="
     + "classpath:application.yml,"
-    + "classpath:application-oauth.yml";
+    + "classpath:application-oauth.yml,"
+    + "classpath:application-datasource.yml";
 
   public static void main(String[] args) {
     new SpringApplicationBuilder(Application.class)
